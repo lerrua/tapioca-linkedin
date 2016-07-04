c37b0b5b50a90b4c53aef02a3852ff6a1c6c8d41
Igor P. Leroy <ip.leroy@gmail.com>
2016-07-04 18:37:59 -0300

    Add public-profile-url param to Linkedin profile

--- tapioca_linkedin/resource_mapping.py
+++ tapioca_linkedin/resource_mapping.py
@@ -2,7 +2,7 @@
 
 RESOURCE_MAPPING = {
     'profile': {
-        'resource': 'people/~:(id,firstName,lastName,headline,summary,positions,num-connections,num-connections-capped,api-standard-profile-request,picture-url,location,industry)?format=json',
+        'resource': 'people/~:(id,firstName,lastName,headline,summary,positions,num-connections,num-connections-capped,api-standard-profile-request,picture-url,location,industry,public-profile-url)?format=json',
         'docs': 'https://developer.linkedin.com/docs/fields/basic-profile'
     },
 }
